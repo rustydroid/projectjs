b09336638a9f8e2da422908e0072f691bf977a82
Marcelo Cardozo <marcelorcardozo@gmail.com>
2022-09-13 12:21:53 -0300

    Se agregan funciones y maqueta en html

--- js/index.js
+++ js/index.js
@@ -1,16 +1,29 @@
-let dolarCotizacion = 269;
+function convertCrypto(option, value) {
+    if (option === 1) {
+        let buyCrypto = value / dolarCotizacion;
+        return buyCrypto.toFixed(2);
+    }
+    if (option === 2) {
+        let sellCrypto = value * dolarCotizacion;
+        return sellCrypto.toFixed(2);
+    }
+}
+
+const dolarCotizacion = 269;
 let opcion = parseInt(prompt("Ingresar opcion 1.COMPRA - 2.VENDE dolares - 3.Salir"));
 
 while (opcion != 3) {
     if (opcion === 1) {
         let cantidadDLS = parseFloat(prompt("Ingrese la cantidad a comprar en AR$"));
-        let compraDLS = cantidadDLS / dolarCotizacion;
-        let result = compraDLS.toFixed(2);
+        // let compraDLS = cantidadDLS / dolarCotizacion;
+        // let result = compraDLS.toFixed(2);
+        let result = convertCrypto(opcion, cantidadDLS);
         alert(`Usted va a recibir ${result} dolares por sus pesos`);
     } else if (opcion === 2) {
         let cantidadDLS = parseFloat(prompt("Ingrese la cantidad a vender en US$"));
-        let ventaDLS = cantidadDLS * dolarCotizacion;
-        let result = ventaDLS.toFixed(2);
+        // let ventaDLS = cantidadDLS * dolarCotizacion;
+        // let result = ventaDLS.toFixed(2);
+        let result = convertCrypto(opcion, cantidadDLS);
         alert(`usted va a recibir ${result} pesos por sus dolares`);
     } else {
         alert("ingreso una opcion incorrecta.");
