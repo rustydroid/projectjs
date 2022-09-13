02ec92c3b2e967644892be8747f5d92b62f9732d
Marcelo Cardozo <marcelorcardozo@gmail.com>
2022-09-13 12:26:03 -0300

    Simulador interactivo

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// Funcion para convertir Dolar crypto / Pesos
 function convertCrypto(option, value) {
     if (option === 1) {
         let buyCrypto = value / dolarCotizacion;
@@ -9,20 +10,17 @@ function convertCrypto(option, value) {
     }
 }
 
+// Programa principal
 const dolarCotizacion = 269;
 let opcion = parseInt(prompt("Ingresar opcion 1.COMPRA - 2.VENDE dolares - 3.Salir"));
 
 while (opcion != 3) {
     if (opcion === 1) {
         let cantidadDLS = parseFloat(prompt("Ingrese la cantidad a comprar en AR$"));
-        // let compraDLS = cantidadDLS / dolarCotizacion;
-        // let result = compraDLS.toFixed(2);
         let result = convertCrypto(opcion, cantidadDLS);
         alert(`Usted va a recibir ${result} dolares por sus pesos`);
     } else if (opcion === 2) {
         let cantidadDLS = parseFloat(prompt("Ingrese la cantidad a vender en US$"));
-        // let ventaDLS = cantidadDLS * dolarCotizacion;
-        // let result = ventaDLS.toFixed(2);
         let result = convertCrypto(opcion, cantidadDLS);
         alert(`usted va a recibir ${result} pesos por sus dolares`);
     } else {
