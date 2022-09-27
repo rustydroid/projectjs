acdea4dedcdbd01d56e579d4903641f49ce4c5ed
Marcelo Cardozo <marcelorcardozo@gmail.com>
2022-09-27 10:05:57 -0300

    Se agrega interactividad con DOM

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-//Const definition
+-0//Const definition
 const exrateBuyUSD = 278;
 const exrateSellUSD = 272;
 
@@ -16,6 +16,7 @@ class Crypto{
 const ARSUSDT = (value, exrateUSD) => value * exrateUSD;
 const ARSADA = (value, exrateUSD) => (value * 0.45) * exrateUSD;
 const ARSETH = (value, exrateUSD) => (value * 1345) * exrateUSD;
+
 
 function convertCrypto(token, value, option) {
     const searchCrypto = cryptos.find(crypto => crypto.callname == token);
@@ -56,7 +57,46 @@ function convertCrypto(token, value, option) {
     }
 }
 
+function updateCart(option, product, description, prodPrice) {
+    let bullet = parseInt((document.getElementById("items-cart").innerText));
+    let total = parseInt(document.getElementById("total").innerText);
+    switch (bullet) {
+        case 0:
+            prod = document.getElementById("item-0-product")
+            desc = document.getElementById("item-0-desc");
+            price = document.getElementById("item-0-price");
+            prod.innerText = product;
+            desc.innerText = description;
+            price.innerText = `AR$ ${prodPrice}`;
+            document.getElementById("items-cart").innerText = 1;
+            document.getElementById("total").innerText = total + prodPrice;
 
+            break;
+        case 1:
+            prod = document.getElementById("item-1-product")
+            desc = document.getElementById("item-1-desc");
+            price = document.getElementById("item-1-price");
+            prod.innerText = product;
+            desc.innerText = description;
+            price.innerText = `AR$ ${prodPrice}`;
+            document.getElementById("items-cart").innerText = 2;
+            document.getElementById("total").innerText = total + prodPrice;
+            break;
+        case 2:
+            prod = document.getElementById("item-2-product")
+            desc = document.getElementById("item-2-desc");
+            price = document.getElementById("item-2-price");
+            prod.innerText = product;
+            desc.innerText = description;
+            price.innerText = `AR$ ${prodPrice}`;
+            document.getElementById("items-cart").innerText = 3;
+            document.getElementById("total").innerText = total + prodPrice;
+            break;
+        default:
+            alert(`Solo se permiten 3 transacciones por sesion`);
+    }
+    
+}
 
 // Programa principal
 const cryptos = [];
@@ -73,12 +113,18 @@ while (option != 3) {
             value = prompt(`Ingrese cantidad de ${token} a comprar`);
             convertion = convertCrypto(token, value, option);
             alert(`La compra de ${token} $${value} equivalen a $${convertion}ARS`);
+            product = `Compra ${token}`;
+            description = `Exchange rate: $${exrateBuyUSD}`;
+            updateCart(option, product, description, convertion);
             break;
         case 2:
             token = prompt("Ingrese el token que desea vender - USDT,ADA,ETH");
             value = prompt(`Ingrese cantidad de ${token} a vender`);
             convertion = convertCrypto(token, value, option);
             alert(`La venta de ${token} $${value} equivalen a $${convertion}ARS`);
+            product = `Venta ${token}`;
+            description = `Exchange rate: $${exrateBuyUSD}`;
+            updateCart(option, product, description, convertion);
             break;
         default:
             alert("ingreso una opcion incorrecta.");
@@ -86,6 +132,8 @@ while (option != 3) {
     }
     option = parseInt(prompt("Ingresar opcion 1.COMPRA - 2.VENTA - 3.Salir"));
 }
+
+
 
 console.log("Cerrando App. Saludos!");
 
