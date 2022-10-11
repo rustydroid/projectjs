c69e328f9978a1ee5238cbb226142f4531e9ecbf
Marcelo Cardozo <marcelorcardozo@gmail.com>
2022-10-11 20:42:26 -0300

    Cambio de interfaz HTML = animaciones

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
--0//Const definition
+//Const definition
 const exrateBuyUSD = 278;
 const exrateSellUSD = 272;
 let cartItems = [];
@@ -22,9 +22,9 @@ const ARSETH = (value, exrateUSD) => (value * 1345) * exrateUSD;
 
 
 function convertCrypto(token, value, option) {
-    const searchCrypto = cryptos.find(crypto => crypto.callname == token);
+    const searchCrypto = cryptos.find(crypto => crypto.callsign == token);
     console.log(`Token encontrado 
-    ${searchCrypto.callname}
+    ${searchCrypto.callsign}
     ${searchCrypto.name}
     ${searchCrypto.exrate}
     ${searchCrypto.stock}`);
@@ -124,7 +124,7 @@ function resetCrypto() {
     action.selectedIndex = 0;
 }
 
-// Programa principal
+// Main Program
 const cryptos = [];
 localStorage.clear();
 cryptos.push(new Crypto("USDT", "USD Theter", 1, 1000));
@@ -133,28 +133,40 @@ cryptos.push(new Crypto("ETH", "Etherum Token", 1345, 200));
 localStorage.setItem("cryptos", JSON.stringify(cryptos));
 
 
-const formCrypto = document.getElementById("form1");
+const credentialValidate = document.getElementById("validate");
+const cartAdd = document.getElementById("cartAdd");
 
-formCrypto.addEventListener("submit", (e) => {
+credentialValidate.addEventListener("click", (e) => {
     e.preventDefault();
+    let credentialSection = document.getElementById("credentialSection");
+    let cryptoSection = document.getElementById("cryptoSection")
+    credentialSection.classList.add("hidden");
+    cryptoSection.classList.remove("hidden");
+    
+});
+
+cartAdd.addEventListener("click", (e) => {
+    let paymentForm = document.getElementById("form2");
+    let cartList = document.getElementById("cartList");
+    paymentForm.classList.remove("hidden");
+    cartList.classList.remove("hidden");
     let origin = document.getElementById("cryptoOrigin").value;
     let target = document.getElementById("cryptoTarget").value;
     let value = parseInt(document.getElementById("amount").value);
     let action = document.getElementById("actionReq").value;
     convertion = convertCrypto(target, value, action);
     if (action === "buy") {
-        console.log("updating Cart buy");
         product = `Compra ${target}`;
         description = `Exchange rate: $${exrateBuyUSD}`;
         updateCart(action, product, description, convertion);
     } else if (action === "sell") {
         product = `Venta ${target}`;
         description = `Exchange rate: $${exrateBuyUSD}`;
+        console.log()
         updateCart(action, product, description, convertion);
     }
     resetCrypto();
-    
-});
+})
 
 
 
